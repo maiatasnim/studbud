d9aa1b61499101cc87c99d8d5e1aebae5080d209
Maia Tasnim <mahiattasnim@gmail.com>
2022-05-12 18:01:35 +1000

    implemented working stopwatch with lap functionality. trouble with tasklist

--- dist/index.99084189.js
+++ dist/index.99084189.js
@@ -1,17 +1,14 @@
 // Basic form DOM elements
 const form = document.getElementById("taskform");
-const button = document.querySelector("#taskform > button")
-
+const button = document.querySelector("#taskform > button");
 // Selector for the tasklist output
 var tasklist = document.querySelector("#tasklist > ul");
-
 // DOM elements for the task input fields
 var taskInput = document.getElementById("taskInput");
 var dueDateInput = document.getElementById("dueDateInput");
 var completionTimeInput = document.getElementById("completionTimeInput");
 var estimatedTimeInput = document.getElementById("estimatedTimeInput");
 var priorityInput = document.getElementById("priorityInput");
-
 // Form submission event listener
 form.addEventListener("submit", function(event) {
     event.preventDefault();
@@ -20,14 +17,10 @@ form.addEventListener("submit", function(event) {
     let completionTime = completionTimeInput.value;
     let estimatedTime = estimatedTimeInput.value;
     let priorityRating = priorityInput.options[priorityInput.selectedIndex].value;
-    if (task) {
-        addTask(task, dueDate, estimatedTime, priorityRating, completionTime, false);
-    }
-})
-
+    if (task) addTask(task, dueDate, estimatedTime, priorityRating, completionTime, false);
+});
 // Create global array to track tasks
 var taskListArray = [];
-
 // Function to add task with user inputs as parameters
 function addTask(taskDescription, dueDate, estimatedTime, priorityRating, completionTime, completionStatus) {
     let d = new Date();
@@ -40,68 +33,55 @@ function addTask(taskDescription, dueDate, estimatedTime, priorityRating, comple
         estimatedTime,
         completionTime,
         priorityRating,
+        estimatedTime,
         completionStatus
     };
     taskListArray.push(task);
     console.log(taskListArray);
     renderTask(task);
 }
-
 // Function to display task on screen
-function renderTask(task) {
-
+function renderTask(task1) {
     // Call function - checks if a task has been added
     updateEmpty();
-
     // Create HTML elements
     let item = document.createElement("li");
-    item.setAttribute("data-id",task.id);
-    item.innerHTML = 
-      "<p>" + task.taskDescription + "</p>" +
-      "<p>" + task.dueDate + "</p>" +
-      "<p>" + task.dateCreated + "</p>"; +
-      "<p>" + task.estimatedTime + "</p>"; +
-      "<p>" + task.completionTime + "</p>"; +
-      "<p>" + task.priorityRating + "</p>"; +
-      "<p>" + task.completionStatus + "</p>"; 
+    item.setAttribute("data-id", task1.id);
+    item.innerHTML = "<p>" + task1.taskDescription + "</p>" + "<p>" + task1.dueDate + "</p>";
+    task1.dueDate;
+    task1.dueDate;
+    task1.dueDate;
+    task1.dueDate;
+    task1.dueDate;
     tasklist.appendChild(item);
-
     // Extra Task DOM elements
     let delButton = document.createElement("button");
     let delButtonText = document.createTextNode("Delete Task");
     delButton.appendChild(delButtonText);
     item.appendChild(delButton);
-
-
     // Event Listeners for DOM elements
     delButton.addEventListener("click", function(event) {
         event.preventDefault();
         let id = event.target.parentElement.getAttribute('data-id');
-        let index = taskListArray.findIndex(task => task.id === Number(id));
-        removeItemFromArray(taskListArray, index)
+        let index = taskListArray.findIndex((task)=>task.id === Number(id)
+        );
+        removeItemFromArray(taskListArray, index);
         console.log(taskListArray);
         updateEmpty();
         item.remove();
-    })
-
+    });
     // Clear the input form
     form.reset();
 }
-
 // Function to remove item from array
 function removeItemFromArray(arr, index) {
-    if (index > -1) {
-        arr.splice(index, 1)
-    }
+    if (index > -1) arr.splice(index, 1);
     return arr;
 }
-
-
 // Function to hide the 'you haven't added any tasks' text
 function updateEmpty() {
-    if (taskListArray.length > 0) {
-        document.getElementById('emptyList').style.display = 'none';
-    } else {
-        document.getElementById('emptyList').style.display = 'block';
-    }
+    if (taskListArray.length > 0) document.getElementById('emptyList').style.display = 'none';
+    else document.getElementById('emptyList').style.display = 'block';
 }
+
+//# sourceMappingURL=index.99084189.js.map
